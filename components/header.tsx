614084b5044deb9ce2df24198ddb9b653756d69f
Loictnl <loictonnelier78@gmail.com>
2024-10-08 14:16:39 +0200

    Ajout de l'url vers le logo sur le cdn de ody

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
     return (
         <header className="h-20 w-full flex items-center px-60 mb-28 bg-surface-primary">
-            <img src="/Logo_Memori.svg" alt="Logo Memori" className="h-6 w-auto" />
+            <img src="https://cdn.ribeiro-ody.com/images/memori/logo_memori.svg" alt="Logo Memori" className="h-6 w-auto" />
         </header>
     );
 };
