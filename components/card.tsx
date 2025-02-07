3ec5728561a532547342ebd6aa0dd2df01d26553
Baptist HECHT <121203663+baptisthecht@users.noreply.github.com>
2025-02-07 20:24:07 +0100

    Update card.tsx

--- components/card.tsx
+++ components/card.tsx
@@ -4,7 +4,7 @@ import { WordDisplay } from "./word-display";
 
 export function Card() {
 	return (
-		<div className="bg-surface-primary rounded-2xl w-1/3 h-80 p-4 flex flex-col items-center justify-between">
+		<div className="bg-surface-primary rounded-2xl w-4/5 sm:w-1/3 h-80 p-4 flex flex-col items-center justify-between">
 			<FilterAndHelper />
 			<WordDisplay />
 			<ActionButtons />
